26ed95e83048745c1cc1d2e63796f4e9a4a4260b
MinotaurosD <doraemon1970@163.com>
2019-04-24 11:47:43 +0800

    Create EnumUtil.cs

---
+++ MinoTool/EnumUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinoTool
+{
+    /// <summary>
+    /// 枚举工具类
+    /// </summary>
+    public static class EnumUtil
+    {
+        /// <summary>
+        /// 获取枚举的名称字符串
+        /// XXX 有装箱
+        /// </summary>
+        /// <typeparam name="T">枚举类型</typeparam>
+        /// <param name="enum">枚举实例</param>
+        /// <returns></returns>
+        public static string GetName<T>(Enum @enum) where T : Enum
+        {
+            return Enum.GetName(typeof(T), @enum);
+        }
+    }
+}
