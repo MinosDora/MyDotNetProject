d02f0c7dc30f53bd2709cedffdb92778d6c5ce41
MinotaurosD <doraemon1970@163.com>
2019-04-28 14:02:00 +0800

    Create EnumTests.cs

---
+++ DotNetTestProject/EnumTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DotNetTestProject
+{
+    /// <summary>
+    /// 枚举
+    /// </summary>
+    public class EnumTests
+    {
+        /// <summary>
+        /// 测试枚举类型编译阶段的操作
+        /// </summary>
+        public void Test1()
+        {
+            MyEnum myEnum = MyEnum.MyEnum1;
+            MyEnum myEnum2 = myEnum | MyEnum.MyEnum2;
+            MyFunc(myEnum2);
+        }
+        public static void MyFunc(MyEnum my)
+        {
+            Console.WriteLine(my);
+        }
+        public enum MyEnum
+        {
+            MyEnum1 = 0x1,
+            MyEnum2 = 0x2,
+        }
+    }
+}
