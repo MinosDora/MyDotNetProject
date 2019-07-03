ecd7ec4f3de15fc064acbbae69120501d502a2e3
MinotaurosD <doraemon1970@163.com>
2019-07-03 10:33:34 +0800

    add covariance tests

--- DotNetTestProject/src/base/CovarianceTests.cs
+++ DotNetTestProject/src/base/CovarianceTests.cs
@@ -12,6 +12,31 @@ namespace DotNetTestProject
         /// </summary>
         public void Test1()
         {
+            object[] myArray = new string[5];
+            IComparable[] myOtherArray = new string[5];
+            try
+            {
+                myArray[0] = 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                myOtherArray[0] = 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 测试数组协变，将派生程度更大的类型的数组隐式转换为派生程度更小的类型的数组后，可能会出现类型不安全的操作
+        /// </summary>
+        public void Test2()
+        {
             try
             {
                 BaseClass[] baseClasses = new Class1[1];
