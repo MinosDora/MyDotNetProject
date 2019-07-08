bf35c24f3b0c7370f4a4fb35435c465ff405297e
MinotaurosD <doraemon1970@163.com>
2019-07-08 11:35:51 +0800

    Update ExpressionBodyTests.cs

--- DotNetTestProject/src/base/ExpressionBodyTests.cs
+++ DotNetTestProject/src/base/ExpressionBodyTests.cs
@@ -4,6 +4,9 @@ namespace DotNetTestProject
 {
     public class ExpressionBodyTests
     {
+        /// <summary>
+        /// 方法表达式主体
+        /// </summary>
         public void CodeSnippet1() => Console.WriteLine("Expression Body");
     }
 }
