276733d5625c7897e619d9ac6eb683bf7ef47252
MinotaurosD <doraemon1970@163.com>
2019-07-09 09:54:39 +0800

    add property tests

---
+++ DotNetTestProject/src/base/PropertyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotNetTestProject
+{
+    /// <summary>
+    /// 属性
+    /// </summary>
+    public class PropertyTests
+    {
+        /// <summary>
+        /// 自动属性在声明时赋值
+        /// </summary>
+        private string MyStr { get; set; } = "Hello";
+
+        /// <summary>
+        /// 只读属性表达式主体
+        /// </summary>
+        private int MyNum => 10;
+    }
+}
