89eba6d6f35c94bf13fa03e487b6dc149ccf55f0
MinotaurosD <doraemon1970@163.com>
2019-07-13 11:07:23 +0800

    Update MethodTests.cs

--- DotNetTestProject/src/base/MethodTests.cs
+++ DotNetTestProject/src/base/MethodTests.cs
@@ -8,6 +8,43 @@ namespace DotNetTestProject
     public class MethodTests
     {
         /// <summary>
+        /// 测试IL指令call和callvir
+        /// </summary>
+        public void Test1()
+        {
+            MyBaseClass myBaseClass = new MyClass();
+            //方法调用                       //IL指令
+            MyBaseClass.MyStaticFunc();     //call
+            myBaseClass.MyFunc1();          //callvirt
+            new MyBaseClass().MyFunc1();    //callvirt
+            myBaseClass.MyFunc2();          //callvirt
+            new MyBaseClass().MyFunc2();    //call
+
+            //下面的方调用在IL代码中是通过callvirt指令调用的，如果使用dnSpy将后者改为call，则结果会不同，借此可理解两个指令的区别
+            myBaseClass.MyFunc1();  //不修改IL指令，结果为MyClass's MyFunc1 is called.
+            myBaseClass.MyFunc1();  //修改为call指令，结果为MyBaseClass's MyFunc1 is called.
+        }
+        class MyBaseClass
+        {
+            public virtual void MyFunc1()
+            {
+                Console.WriteLine($"{nameof(MyBaseClass)}'s {nameof(MyFunc1)} is called.");
+            }
+            public void MyFunc2()
+            {
+                Console.WriteLine($"{nameof(MyBaseClass)}'s {nameof(MyFunc2)} is called.");
+            }
+            public static void MyStaticFunc() { }
+        }
+        class MyClass : MyBaseClass
+        {
+            public override void MyFunc1()
+            {
+                Console.WriteLine($"{nameof(MyClass)}'s {nameof(MyFunc1)} is called.");
+            }
+        }
+
+        /// <summary>
         /// 局部函数Local Function
         /// </summary>
         public void CodeSnippet1()
