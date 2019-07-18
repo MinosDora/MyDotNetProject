5b5cc0d00681bead5bc600fe41cff1eb1741c854
MinotaurosD <doraemon1970@163.com>
2019-07-18 09:54:13 +0800

    Update PrimitiveTypeTests.cs

--- DotNetTestProject/src/base/PrimitiveTypeTests.cs
+++ DotNetTestProject/src/base/PrimitiveTypeTests.cs
@@ -13,9 +13,9 @@ namespace DotNetTestProject
         public void Test1()
         {
             char charA = 'A', charB = (char)65;
-            Console.WriteLine(charA == charB);
-            Console.WriteLine(charA + 1);
-            Console.WriteLine((char)(charA + 1));
+            Console.WriteLine(charA == charB);      //True
+            Console.WriteLine(charA + 1);           //66，char和int相加，char会隐式转换为int，然后执行相加操作，返回int
+            Console.WriteLine((char)(charA + 1));   //B，接上，返回的int值强制转换为char类型输出
         }
     }
 }
