7c840b62dd5d69976d8fee6c7effc534660c4c03
MinotaurosD <doraemon1970@163.com>
2019-07-23 11:37:25 +0800

    Update VariableTests.cs

--- DotNetTestProject/src/base/VariableTests.cs
+++ DotNetTestProject/src/base/VariableTests.cs
@@ -8,7 +8,7 @@ namespace DotNetTestProject
     public class VariableTests
     {
         /// <summary>
-        /// 测试局部变量未初始化时即已分配内存，未赋值的局部变量不能直接访问是编译器为防止程序员粗心造成Bug而添加的错误信息
+        /// 测试局部变量未初始化时即已在栈内存中分配内存，未赋值的局部变量不能直接访问是编译器为防止程序员粗心造成Bug而添加的错误信息
         /// </summary>
         public unsafe void Test1()
         {
@@ -19,6 +19,7 @@ namespace DotNetTestProject
             Console.WriteLine((int)&b);     //-1233195480
             Console.WriteLine((int)&c);     //-1233195488
             Console.WriteLine(*(&b));       //0
+            Console.WriteLine(b);           //0，取址操作后，编译器不再进行值类型局部变量是否初始化的验证
         }
     }
 }
